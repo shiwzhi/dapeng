1fd81bdb1b4e1416029ce0030d1bbc49dc548f40
William Shi <shiwzhi@gmail.com>
2020-07-10 22:35:50 +0800

    test mesh

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,19 @@
 #include <WiFiClient.h>
 #include <Wire.h>
 #include "SHTSensor.h"
-#include <ArduinoJson.h>
+// #include <ArduinoJson.h>
 #include <ESP8266httpUpdate.h>
+#include <painlessMesh.h>
+
+#define MESH_PREFIX "shiweizhi"
+#define MESH_PASSWORD "shiwzhi"
+#define MESH_PORT 6666
+
+#define STATION_SSID "HOME"
+#define STATION_PASSWORD "12345679"
+#define HOSTNAME "WiFi Bridge"
+
+painlessMesh mesh;
 
 const char *version = "1.4";
 
@@ -19,8 +30,11 @@ const int httpsPort = 9527;
 IPAddress staticIP(192, 168, 2, 200);
 IPAddress gateway(192, 168, 2, 1);
 IPAddress subnet(255, 255, 255, 0);
-IPAddress dns(223,5,5,5);
-IPAddress dns1(223,6,6,6);
+IPAddress dns(223, 5, 5, 5);
+IPAddress dns1(223, 6, 6, 6);
+
+IPAddress myIP(0,0,0,0);
+
 
 SHTSensor sht;
 int sht_vcc = D5;
@@ -67,149 +81,154 @@ void update_error(int err)
   Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
 }
 
+void receivedCallback(const uint32_t &from, const String &msg)
+{
+  Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
+}
+
 void setup()
 {
 
   Serial.begin(115200);
-
-  uint32 rtc_data;
-  String a;
-
-  ESP.rtcUserMemoryRead(100, &rtc_data, sizeof(rtc_data));
-
-  Serial.println(rtc_data);
-
-  if (rtc_data != 52011)
-  {
-    Serial.println("write rtc");
-    rtc_data = 52011;
-    ESP.rtcUserMemoryWrite(100, &rtc_data, sizeof(rtc_data));
-  }
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 1);
+  mesh.onReceive(&receivedCallback);
+  mesh.stationManual(STATION_SSID, STATION_PASSWORD);
+  mesh.setHostname(HOSTNAME);
+  mesh.setRoot(true);
+  mesh.setContainsRoot(true);
 
   int soilMoisture;
-  for(int i = 0; i<5; i++)
+  for (int i = 0; i < 5; i++)
   {
     soilMoisture = analogRead(A0);
   }
   Serial.println(soilMoisture);
   soilMoisture = map(soilMoisture, 1024, 3, 0, 100);
   Serial.println(soilMoisture);
-  
+
   //ESP.deepSleep(ESP.deepSleepMax());
 
-  WiFi.config(staticIP, gateway, subnet, dns, dns1);
-  WiFi.begin(ssid, pass);
+  // WiFi.config(staticIP, gateway, subnet, dns, dns1);
+  // WiFi.begin(ssid, pass);
   pinMode(sht_vcc, OUTPUT);
   digitalWrite(sht_vcc, HIGH);
   Wire.begin(D1, D2);
 
-  if (sht.init())
-  {
-    Serial.print("sht init(): success\n");
-  }
-  else
-  {
-    Serial.print("sht init(): failed\n");
-    goSleep(5 * 60);
-  }
+  // if (sht.init())
+  // {
+  //   Serial.print("sht init(): success\n");
+  // }
+  // else
+  // {
+  //   Serial.print("sht init(): failed\n");
+  //   goSleep(5 * 60);
+  // }
 
-  sht.setAccuracy(SHTSensor::SHT_ACCURACY_HIGH);
+  // sht.setAccuracy(SHTSensor::SHT_ACCURACY_HIGH);
 
-  sht30_measure();
+  // sht30_measure();
 
-  int wifiRetry = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.print(".");
-    wifiRetry++;
-    if (wifiRetry > 60)
-    {
-      goSleep(5 * 60);
-    }
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  // int wifiRetry = 0;
+  // while (WiFi.status() != WL_CONNECTED)
+  // {
+  //   delay(1000);
+  //   Serial.print(".");
+  //   wifiRetry++;
+  //   if (wifiRetry > 60)
+  //   {
+  //     goSleep(5 * 60);
+  //   }
+  // }
+  // Serial.println("");
+  // Serial.println("WiFi connected");
+  // Serial.println("IP address: ");
+  // Serial.println(WiFi.localIP());
 
   WiFiClient client;
 
-  if (!client.connect(host, httpsPort))
-  {
-    Serial.println("connection failed");
-    goSleep(5 * 60);
-  }
+  // if (!client.connect(host, httpsPort))
+  // {
+  //   Serial.println("connection failed");
+  //   goSleep(5 * 60);
+  // }
 
-  const size_t capacity = JSON_OBJECT_SIZE(6);
-  DynamicJsonDocument doc(capacity);
+  // const size_t capacity = JSON_OBJECT_SIZE(6);
+  // DynamicJsonDocument doc(capacity);
 
-  doc["temp"] = temp;
-  doc["hum"] = hum;
-  doc["id"] = String(ESP.getChipId());
-  doc["soilMois"] = soilMoisture;
-  doc["version"] = version;
-  char buffer[121];
+  // doc["temp"] = temp;
+  // doc["hum"] = hum;
+  // doc["id"] = String(ESP.getChipId());
+  // doc["soilMois"] = soilMoisture;
+  // doc["version"] = version;
+  // char buffer[121];
 
-  serializeJson(doc, buffer);
+  // serializeJson(doc, buffer);
 
-  client.print(buffer);
+  // client.print(buffer);
 
-  Serial.println("request sent");
+  // Serial.println("request sent");
 
-  unsigned long timeout = millis();
-  while (client.available() == 0)
-  {
-    if (millis() - timeout > 5000)
-    {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      goSleep(5 * 60);
-    }
-  }
+  // unsigned long timeout = millis();
+  // while (client.available() == 0)
+  // {
+  //   if (millis() - timeout > 5000)
+  //   {
+  //     Serial.println(">>> Client Timeout !");
+  //     client.stop();
+  //     goSleep(5 * 60);
+  //   }
+  // }
 
-  uint8 sleep = client.read();
-  uint8 ota = client.read();
-  Serial.println(sleep);
-  Serial.println(ota);
-  // Close the connection
-  Serial.println();
+  // uint8 sleep = client.read();
+  // uint8 ota = client.read();
+  // Serial.println(sleep);
+  // Serial.println(ota);
+  // // Close the connection
+  // Serial.println();
 
-  if (ota)
-  {
-    ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
-    ESPhttpUpdate.onStart(update_started);
-    ESPhttpUpdate.onEnd(update_finished);
-    ESPhttpUpdate.onProgress(update_progress);
-    ESPhttpUpdate.onError(update_error);
+  // if (ota)
+  // {
+  //   ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+  //   ESPhttpUpdate.onStart(update_started);
+  //   ESPhttpUpdate.onEnd(update_finished);
+  //   ESPhttpUpdate.onProgress(update_progress);
+  //   ESPhttpUpdate.onError(update_error);
 
-    WiFiClientSecure otaClient;
-    otaClient.setInsecure();
+  //   WiFiClientSecure otaClient;
+  //   otaClient.setInsecure();
 
-    t_httpUpdate_return ret = ESPhttpUpdate.update(otaClient, "https://moe.swz1994.xyz:1880/arduino/ota", version);
-    switch (ret)
-    {
-    case HTTP_UPDATE_FAILED:
-      Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
-      break;
+  //   t_httpUpdate_return ret = ESPhttpUpdate.update(otaClient, "https://moe.swz1994.xyz:1880/arduino/ota", version);
+  //   switch (ret)
+  //   {
+  //   case HTTP_UPDATE_FAILED:
+  //     Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+  //     break;
 
-    case HTTP_UPDATE_NO_UPDATES:
-      Serial.println("HTTP_UPDATE_NO_UPDATES");
-      break;
+  //   case HTTP_UPDATE_NO_UPDATES:
+  //     Serial.println("HTTP_UPDATE_NO_UPDATES");
+  //     break;
 
-    case HTTP_UPDATE_OK:
-      Serial.println("HTTP_UPDATE_OK");
-      break;
-    }
+  //   case HTTP_UPDATE_OK:
+  //     Serial.println("HTTP_UPDATE_OK");
+  //     break;
+  //   }
 
-    Serial.println("closing connection");
-  }
+  //   Serial.println("closing connection");
+  // }
 
-  client.stop();
-  goSleep(sleep * 60);
+  // client.stop();
+  // goSleep(sleep * 60);
 }
 
 void loop()
 {
-  // timer1.update();
+  mesh.update();
+
+  if (myIP != IPAddress(mesh.getStationIP())) {
+    myIP = IPAddress(mesh.getStationIP());
+    Serial.println("My IP is " + myIP.toString());
+  }
+
+
 }
