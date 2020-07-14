a0ce44b249594b0a4cd51a3394338e63088200ec
William Shi <shiwzhi@gmail.com>
2020-07-14 13:46:54 +0800

    Use wifi multi

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #include <WiFiClientSecure.h>
 #include <WiFiClient.h>
 #include <Wire.h>
@@ -8,12 +9,10 @@
 #include <ArduinoJson.h>
 #include <ESP8266httpUpdate.h>
 
-const char *version = "1.5";
+const char *version = "1.6";
 
 ADC_MODE(ADC_VCC);
 
-const char ssid[] = "wanlaoshi";
-const char pass[] = "jiejiemomo";
 const char *host = "moe.swz1994.xyz";
 const int httpsPort = 9527;
 
@@ -30,6 +29,8 @@ float temp = 0;
 float hum = 0;
 
 ulong start_time;
+
+ESP8266WiFiMulti wifiMulti;
 
 void goSleep(int sec)
 {
@@ -86,8 +87,17 @@ void setup()
     
   }
 
-  WiFi.config(staticIP, gateway, subnet, dns, dns1);
-  WiFi.begin(ssid, pass);
+  WiFi.mode(WIFI_STA);
+
+  // WiFi.config(staticIP, gateway, subnet, dns, dns1);
+  // WiFi.begin(ssid, pass);
+
+  wifiMulti.addAP("HOME", "12345679");
+  wifiMulti.addAP("wanlaoshi", "jiejiemomo");
+  wifiMulti.addAP("OnePlus7", "12345679");
+
+
+
   pinMode(sht_vcc, OUTPUT);
   digitalWrite(sht_vcc, HIGH);
   Wire.begin(D1, D2);
@@ -107,9 +117,9 @@ void setup()
   sht30_measure();
 
   int wifiRetry = 0;
-  while (WiFi.status() != WL_CONNECTED)
+  while (wifiMulti.run() != WL_CONNECTED)
   {
-    delay(1000);
+    delay(500);
     Serial.print(".");
     wifiRetry++;
     if (wifiRetry > 60)
@@ -161,7 +171,7 @@ void setup()
 
   uint8 sleep = 5;
   uint8 sleep_read = client.read();
-  if (sleep_read != -1) {
+  if (sleep_read != -1 && sleep_read > 0) {
     sleep = sleep_read;
   }
   
@@ -212,5 +222,6 @@ void loop()
   if (millis() - start_time > 60*1000) {
     goSleep(5*60);
   }
+  delay(1000);
   // timer1.update();
 }
