d04f89b37275bf9e651db36cd6499579e3b5e63c
William Shi <shiwzhi@gmail.com>
2020-07-26 08:07:49 +0800

    modify dht retry time

--- src/run_dht.cpp
+++ src/run_dht.cpp
@@ -1,8 +1,8 @@
 #include "run_dht.h"
 
-DHT dht(D2, DHT11);
+DHT dht(4, DHT11);
 
-int dht_vcc = D1;
+int dht_vcc = 5;
 
 float hum = 0;
 float temp = 0;
@@ -21,11 +21,11 @@ bool run_dht()
     uint dht_retry = 0;
     while (isnan(temp) || isnan(hum))
     {
-        delay(100);
+        delay(1000);
         hum = dht.readHumidity();
         temp = dht.readTemperature();
         dht_retry++;
-        if (dht_retry > 50)
+        if (dht_retry > 20)
         {
             Serial.println("DHT Failed");
             return false;
