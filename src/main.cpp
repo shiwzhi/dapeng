9b3ad9489e2bd2f2ce4599e40d15970a2c7f86e6
William Shi <shiwzhi@gmail.com>
2020-07-31 20:48:48 +0800

    add ds18b20

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "run_ota.h"
 #include "run_upload.h"
 #include "run_sht30.h"
+#include "run_ds18b20.h"
 
 ADC_MODE(ADC_VCC);
 const char* version = "2.0.2";
@@ -33,6 +34,9 @@ void setup()
         goSleepSec(5 * 60);
     }
 
+    if (!run_ds18b20()) {
+        goSleepSec(5 * 60);
+    }
 
     if (!upload_tcp(readTemp(), readHum(), "SHT30"))
     {
